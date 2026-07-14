b2a929b5b867b81e877b1eb31feb7a2977f769b9
Rodrigo Hernandez <fixer130404@gmail.com>
2026-07-13 21:48:39 -0600

    Degradado de colores.

--- app/src/main/java/com/example/proyeto2/ui/theme/Color.kt
+++ app/src/main/java/com/example/proyeto2/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.example.proyeto2.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -18,3 +19,19 @@ val mostasa = Color(red = 242, green = 204, blue = 143)
 val oscuro = Color(red = 31, green = 41, blue = 55)
 val medio = Color(red = 107, green = 114, blue = 128)
 val neutro = Color(red = 34, green = 85, blue = 102)
+
+val GradientTierra = Brush.linearGradient(listOf(coffe, terracota, mostasa))
+val GradientNaturaleza = Brush.linearGradient(listOf(oscuro, neutro, oliva))
+val GradientSofisticado = Brush.linearGradient(listOf(carbon, medio, oscuro))
+val GradientOtono = Brush.linearGradient(listOf(terracota, mostasa))
+val GradientProfundo = Brush.linearGradient(listOf(coffe, oliva))
+
+val GradientAurora = Brush.linearGradient(listOf(oscuro, neutro, mostasa)) // De la oscuridad al amanecer
+val GradientCrepusculo = Brush.linearGradient(listOf(oscuro, terracota, coffe)) // El cielo enfriándose tras el calor
+val GradientEstatico = Brush.linearGradient(listOf(carbon, medio, neutro)) // Frialdad industrial y concreto
+val GradientVital = Brush.linearGradient(listOf(oliva, mostasa, terracota)) // La energía de la vegetación y el sol
+val GradientClaroOscuro = Brush.linearGradient(listOf(mostasa, carbon)) // Contraste máximo de luminosidad
+
+val GradientTotal = Brush.linearGradient(
+    listOf(oscuro, coffe, carbon, neutro, oliva, medio, terracota, mostasa)
+)
